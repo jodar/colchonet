048b9f72d218fea972e2a9f9eded5551394cb7ad
Luan Jodar <luan.jodar@gmaiil.com>
2015-01-06 17:35:58 -0200

    Foi adicionado BCrypt, para criptografias de senhas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150105192836) do
+ActiveRecord::Schema.define(version: 20150106191304) do
 
   create_table "rooms", force: true do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20150105192836) do
     t.text     "bio"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
